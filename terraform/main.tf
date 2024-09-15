54f10b6e9a8efa3cfbfb8bbad6648571c40dfe50
Ruslan Gusseinov <r.gusseinov@mail.ru>
2024-09-15 21:09:49 +0500

    fix ssh-key path

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "yandex_compute_instance" "web" {
   connection {
     type        = "ssh"
     user        = "ruslan"
-    private_key = file("${path.module}/id_rsa.pub")
+    private_key = file("~/.ssh/id_ed25519")
     host        = self.network_interface.0.nat_ip_address
   }
 
