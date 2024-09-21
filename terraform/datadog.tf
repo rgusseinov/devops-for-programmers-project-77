870e04de1b465fb21a395b8801359ec83a0df6fa
Ruslan <r.gusseinov@mail.ru>
2024-09-21 21:49:32 +0500

    move datadog provider in provider file

--- terraform/datadog.tf
+++ terraform/datadog.tf
@@ -1,11 +1,3 @@
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = "https://us5.datadoghq.com"
-
-}
-
-
 resource "datadog_monitor" "foo" {
   name               = "Ruslan Hexlet for monitor foo"
   type               = "metric alert"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,3 +17,9 @@ provider "yandex" {
   folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
 }
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://us5.datadoghq.com"
+}
