d8126cb59a9060d47705e226b72ba2629f27e3e7
Ruslan <r.gusseinov@mail.ru>
2024-09-21 23:55:37 +0500

    add datadog config

--- terraform/datadog.tf
+++ terraform/datadog.tf
@@ -13,3 +13,11 @@ resource "datadog_monitor" "foo" {
     critical_recovery = 3
   }
 }
+
+resource "datadog_monitor" "healthcheck" {
+  name    = "Healthcheck monitor - ${uuid()}"
+  type    = "service check"
+  query   = "\"http.can_connect\".over(\"instance:datadog_health_check\").by(\"host\",\"instance\",\"url\").last(4).count_by_status()"
+  message = "{{host.name}} not available"
+}
+
